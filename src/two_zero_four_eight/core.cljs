960631c5e35577352b46a201bd4ef0a591991c93
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2020-03-22 12:26:29 +0530

    Added keybord events

--- src/two_zero_four_eight/core.cljs
+++ src/two_zero_four_eight/core.cljs
@@ -1,7 +1,35 @@
 (ns two_zero_four_eight.core
-    (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
+
+(defn append-zeroes
+  [size coll]
+  (take size (concat coll (repeat 0))))
+
+(def move-left-row-without-zeroes
+  (comp
+    (partial map #(apply + %))
+    (partial mapcat #(partition-all 2 %))
+    (partial partition-by identity)
+    (partial #(filter pos? %))))
+
+(def move-left-row
+  (comp
+    #(apply append-zeroes %)
+    (juxt count move-left-row-without-zeroes)))
+
+(def reverse-board #(map reverse %))
+
+(def transpose #(apply map list %))
+
+(def move-board-left #(map move-left-row %))
+
+(def move-board-right (comp reverse-board move-board-left reverse-board))
+
+(def move-board-up (comp transpose move-board-left transpose))
+
+(def move-board-down (comp transpose move-board-right transpose))
 
 (defonce state (reagent/atom '((:EMPTY :EMPTY :EMPTY 2)
                                (:EMPTY :EMPTY :EMPTY :EMPTY)
@@ -15,7 +43,14 @@
    (map create-cell row)])
 
 (defn board []
-  [:div {:class "board"}
+  [:div {:class       "board"
+         :tabIndex    -1
+         :on-key-down #(case (.-which %)
+                         38 (swap! state move-board-up)
+                         40 (swap! state move-board-down)
+                         37 (swap! state move-board-left)
+                         39 (swap! state move-board-right)
+                         nil)}
    (map create-row @state)])
 
 (reagent/render-component [board]
